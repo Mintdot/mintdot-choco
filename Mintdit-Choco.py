fa482ce30ebb96d25ba76b7b0170f9c395803904
nigth <nigthmare100@gmail.com>
2018-06-05 16:08:38 +0900

    1데이 1코 기초 코드 완성

--- Mintdit-Choco.py
+++ Mintdit-Choco.py
@@ -1,1 +1,25 @@
-print("test")
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+token = 'NDUzNDM5NTQwMTg3ODg5Njg1.Dfe6Bg.AB8nqe01dXqmQCU2cHbN1XfRbNs'
+
+bot = commands.Bot(command_prefix='-co ')
+
+@bot.event
+async def on_ready():
+    print("CHOCO BOT IS READY")
+    print("BOT NAME: " + bot.user.name)
+    print("BOT ID: " + bot.user.id)
+    await bot.change_presence(game=discord.Game(name="-co co"))
+
+@bot.command(pass_context=True)
+async def co(ctx):
+    embed = discord.Embed(title="Let's co's time.")
+    embed.set_author(name="1DAY 1CO", icon_url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
+    embed.set_image(url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
+    embed.set_footer(text="co is best dog", icon_url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
+    await bot.say(embed=embed)
+
+bot.run(token)

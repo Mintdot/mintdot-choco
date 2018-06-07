ec38cc3c690ed28f8577037e2afca06b47a8eef6
nigth <nigthmare100@gmail.com>
2018-06-07 15:39:56 +0900

    game

--- Mintdot-Choco.py
+++ Mintdot-Choco.py
@@ -12,7 +12,7 @@ async def on_ready():
     print("CHOCO BOT IS READY")
     print("BOT NAME: " + bot.user.name)
     print("BOT ID: " + str(bot.user.id))
-    await bot.change_presence(game=discord.Game(name="-co co"))
+    await bot.change_presence(game="-co co")
 
 @bot.command(pass_context=True)
 async def co(ctx):

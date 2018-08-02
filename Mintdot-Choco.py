27ed2756e2002ff57722fe6371af09fcfb6c6aa1
nigth <nigthmare100@gmail.com>
2018-08-03 03:55:47 +0900

    초심으로

--- Mintdot-Choco.py
+++ Mintdot-Choco.py
@@ -5,8 +5,6 @@ import asyncio
 
 token = 'NDUzNDM5NTQwMTg3ODg5Njg1.DfpOCg.tkNCrOVrWvMP8Mrh9ejW8-QwojE'
 
-co = '초코'
-
 bot = commands.Bot(command_prefix='!!')
 
 @bot.event
@@ -18,21 +16,13 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     # await bot.change_presence(game=discord.Game(name="!!"))
 
-@bot.command(pass_context=True, name='냐옹')
-async def co_default(ctx):
+@bot.command(pass_context=True, name='초코')
+async def co(ctx):
     embed = discord.Embed(title="Let's co's time.")
     embed.set_author(name="1DAY 1CO", icon_url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
     embed.set_image(url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
     embed.set_footer(text="co is best dog", icon_url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
     await ctx.send(embed=embed)
-
-@bot.command(pass_context=True, name='초코 손!')
-async def co_hand(ctx, *, args):
-    await ctx.send('왈!')
-
-@bot.command(pass_context=True, name=co+' 발!')
-async def co_hand(ctx):
-    await ctx.send('왈!')
 
 @bot.command(pass_context=True, name='메이플왜해요')
 async def maple(ctx):

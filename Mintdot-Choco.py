8ad6bb39269a2cc16cf8fb07355dfb0ddbe9596c
nigth <nigthmare100@gmail.com>
2018-08-03 03:16:16 +0900

    함수명 변경

--- Mintdot-Choco.py
+++ Mintdot-Choco.py
@@ -19,18 +19,18 @@ async def on_ready():
     # await bot.change_presence(game=discord.Game(name="!!"))
 
 @bot.command(pass_context=True, name='초코')
-async def co(ctx):
+async def co_default(ctx):
     embed = discord.Embed(title="Let's co's time.")
     embed.set_author(name="1DAY 1CO", icon_url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
     embed.set_image(url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
     embed.set_footer(text="co is best dog", icon_url="https://raw.githubusercontent.com/Mintdot/Mintdot-Choco/master/img/co.png")
     await ctx.send(embed=embed)
 
-@bot.command(pass_context=True, name=(co + '손!'))
+@bot.command(pass_context=True, name=co+' 손!')
 async def co_hand(ctx):
     await ctx.send('왈!')
 
-@bot.command(pass_context=True, name='초코 발!')
+@bot.command(pass_context=True, name=co+' 발!')
 async def co_hand(ctx):
     await ctx.send('왈!')
 
